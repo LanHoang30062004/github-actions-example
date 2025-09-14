726a290bc4f2dbb8b8504fac728c7aa32047abb0
MaiHoangLan <maihoanglan30@gmail.com>
2025-09-14 20:14:40 +0700

    first commit

---
+++ src/test/java/com/mahala/cicd/CiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahala.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
